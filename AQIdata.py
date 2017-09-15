27ec3c4c102236d873ff0a98a062fe6de595cd33
Luxury <lx644966783@sina.com>
2017-09-15 11:01:59 +0800

    AQIdataspyder 2.0

--- AQIdata.py
+++ AQIdata.py
@@ -13,11 +13,10 @@ import re
 import numpy as np
 import lxml.etree
 import xlutils.copy
-
 # 初始化所用列表
 DATA = []                   # 实时数据
 DayData = []                # 每日数据
-Index = ['AQI','SO2','NO2','PM10','CO2','O3','PM2.5']           # 空气质量指标
+Index = ['AQI','SO2','NO2','PM10','CO','O3','PM2.5']           # 空气质量指标
 DayDataXcagtegories=["青羊区","金牛区","锦江区","武侯区","成华区","高新区", "龙泉驿区",       # 成都地区
                      "青白江区","新都区","温江区","双流区","郫县","天府新区","都江堰市",
                      "崇州市","新津县","彭州市","邛崃市","大邑县","蒲江县"]
@@ -26,22 +25,39 @@ first_url = 'http://www.cdepb.gov.cn/cdepbws/web/gov/airquality.aspx'
 r = requests.get(first_url)
 html = r.text
 
-# 获取当前时间
+# 使用xpath获取当前时间,AQI等数据
 data_time = lxml.etree.HTML(html).xpath('//*[@id="ContentBody_AQITime"]/text()')
 data_time = str(data_time).replace(' ','').replace('[u\'','').replace('\']','').replace\
     ('\u5e74','.').replace('\u6708','.').replace('\u65e5','.').replace('\u65f6','')
+data_AQI = lxml.etree.HTML(html).xpath('//*[@id="ContentBody_AqiData"]/text()')[0]
+data_play = str(lxml.etree.HTML(html).xpath('//*[@id="ContentBody_FirstPoll"]/text()'))\
+    .replace('[u\'','').replace('\']','').replace('\u9996\u8981\u6c61\u67d3\u7269\uff1a','')
+data_SO2 = lxml.etree.HTML(html).xpath('//*[@id="ContentBody_SO2IAQI"]/text()')[0]
+data_NO2 = lxml.etree.HTML(html).xpath('//*[@id="ContentBody_NO2IAQI"]/text()')[0]
+data_PM10 = lxml.etree.HTML(html).xpath('//*[@id="ContentBody_PM10IAQI"]/text()')[0]
+data_NO = lxml.etree.HTML(html).xpath('//*[@id="ContentBody_CO1IAQI"]/text()')[0]
+data_O3 = lxml.etree.HTML(html).xpath('//*[@id="ContentBody_O3IAQI"]/text()')[0]
+data_PM25 = lxml.etree.HTML(html).xpath('//*[@id="ContentBody_PM25IAQI"]/text()')[0]
+AQIDaydata = [data_AQI,data_play,data_SO2, data_NO2, data_PM10, data_NO,data_O3, data_PM25, data_time ]                         # 每日8点AQI总体显示
 
 # 获取地区记录时间
 data_time2 = re.findall(r'inline-block;" value.*? />', html, re.S)[0]
 data_time2 = re.findall(r'value.*/>',data_time2)[0]
 data_time2 = re.findall(r'".*"',data_time2)[0].replace('\"','')
 
-# 使用xlrd 与 xlutils.copy 对数据进行追加操作
-dayDatabookopen = xlrd.open_workbook(r'AQI.xls')
+# 使用xlrd 与 xlutils.copy 对数据进行追加操作，操作 /AQI.xlsx 表
+dayDatabookopen = xlrd.open_workbook(r'D:/CGI/python/note/AQIspyder/AQI.xlsx')
 sheet = dayDatabookopen.sheet_by_index(0)
 col = sheet.col_values(0)                       # 获取第一列
 newdayDatabookopen = xlutils.copy.copy(dayDatabookopen)
 newsheet = newdayDatabookopen.get_sheet(0)
+
+#   使用xlrd 与 xlutils.copy 对数据进行追加操作,操作 /环检测数据.xlsx 表
+AQIdayDatabook = xlrd.open_workbook(r'D:/CGI/python/note/AQIspyder/AQI2.xlsx')
+AQIsheet = AQIdayDatabook.sheet_by_index(0)
+col2 = AQIsheet.col_values(0)                       # 获取第一列
+newAQIdayDatabook = xlutils.copy.copy(AQIdayDatabook)
+newAQIsheet = newAQIdayDatabook.get_sheet(0)
 
 # 使用xlwt 设定编码方式
 workbook=xlwt.Workbook(encoding='utf-8')
@@ -60,6 +76,8 @@ booksheet6=workbook.add_sheet('沙河滩', cell_overwrite_ok=True)
 booksheet7=workbook.add_sheet('三瓦窑', cell_overwrite_ok=True)
 booksheet8=workbook.add_sheet('十里店', cell_overwrite_ok=True)
 booksheet9=workbook.add_sheet('灵岩山', cell_overwrite_ok=True)
+
+booksheet = [booksheet1, booksheet2, booksheet3, booksheet4, booksheet5, booksheet6, booksheet7, booksheet8, booksheet9] 
 
 # 获取地区实时数据
 content = re.findall(r'monitorChartJson=.*diviChartJson=', html, re.S )[0]
@@ -92,67 +110,25 @@ for i in range(len(DayDataXcagtegories)):
     newsheet.write(len(col)+1,0,data_time2)
     newsheet.write(len(col)+1,i+1,DayData[i])
 
+# 写入表格AQI总体数据
+for i in range(len(AQIDaydata)):
+    newAQIsheet.write(len(col2),i,AQIDaydata[i])
+
 # 写入表格实时数据 为四维数据
-for i in range(7):
-    for j in range(24):
-        booksheet1.write(0,j+1,j)
-        booksheet1.write(i+1,0,Index[i])
-        booksheet1.write(i+1,j+1,DATA[0][i][j])
+for k in range(len(booksheet)):
+    for i in range(7):
+        for j in range(24):
+            booksheet[k].write(0,j+1,j)
+            booksheet[k].write(i+1,0,Index[i])
+            booksheet[k].write(i+1,j+1,DATA[k][i][j])
 
-for i in range(7):
-    for j in range(24):
-        booksheet2.write(0,j+1,j)
-        booksheet2.write(i+1,0,Index[i])
-        booksheet2.write(i+1,j+1,DATA[1][i][j])
-
-for i in range(7):
-    for j in range(24):
-        booksheet3.write(0,j+1,j)
-        booksheet3.write(i+1,0,Index[i])
-        booksheet3.write(i+1,j+1,DATA[2][i][j])
-
-for i in range(7):
-    for j in range(24):
-        booksheet4.write(0,j+1,j)
-        booksheet4.write(i+1,0,Index[i])
-        booksheet4.write(i+1,j+1,DATA[3][i][j])
-
-for i in range(7):
-    for j in range(24):
-        booksheet5.write(0,j+1,j)
-        booksheet5.write(i+1,0,Index[i])
-        booksheet5.write(i+1,j+1,DATA[4][i][j])
-
-for i in range(7):
-    for j in range(24):
-        booksheet6.write(0,j+1,j)
-        booksheet6.write(i+1,0,Index[i])
-        booksheet6.write(i+1,j+1,DATA[5][i][j])
-
-for i in range(7):
-    for j in range(24):
-        booksheet7.write(0,j+1,j)
-        booksheet7.write(i+1,0,Index[i])
-        booksheet7.write(i+1,j+1,DATA[6][i][j])
-
-for i in range(7):
-    for j in range(24):
-        booksheet8.write(0,j+1,j)
-        booksheet8.write(i+1,0,Index[i])
-        booksheet8.write(i+1,j+1,DATA[7][i][j])
-
-for i in range(7):
-    for j in range(24):
-        booksheet9.write(0,j+1,j)
-        booksheet9.write(i+1,0,Index[i])
-        booksheet9.write(i+1,j+1,DATA[8][i][j])
 
 # 以时间为表格名保存实时数据
-workbook.save('%s.xls' % data_time)
+workbook.save('D:/CGI/python/note/AQIspyder/data/%s.xlsx' % data_time)
 
 # 保存每日数据
-newdayDatabookopen.save('AQI.xls')
-
+newdayDatabookopen.save('D:/CGI/python/note/AQIspyder/AQI.xlsx')
+newAQIdayDatabook.save('D:/CGI/python/note/AQIspyder/AQI2.xlsx')
 # 显示是否上传成功
 print(u'%s 数据已上传至表格' % data_time)
 
